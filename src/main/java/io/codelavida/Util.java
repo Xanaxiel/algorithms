c353b1a365fb6cfccf3a34dcda109f608be135bf
Rabi Panda <rabipanda@icloud.com>
2018-07-02 23:57:37 +1000

    Calculate median in an array of integers

--- src/main/java/io/codelavida/Util.java
+++ src/main/java/io/codelavida/Util.java
@@ -2,7 +2,17 @@ package io.codelavida;
 
 import java.util.Random;
 
-public class Util {
+/**
+ * Collection of utility functions
+ */
+public final class Util {
+
+    private static Random generator = new java.util.Random();
+
+    private Util() {
+
+    }
+
     /**
      * Checks if an array of integers is sorted in non-decreasing order
      *
@@ -18,6 +28,13 @@ public class Util {
         return true;
     }
 
+    /**
+     * Checks if an array of reference types is sorted in non-decreasing order.
+     * The class must implement the Comparable interface.
+     *
+     * @param arr input array of objects
+     * @return true if the array is sorted or false
+     */
     public static boolean isSorted(Comparable[] arr) {
         for (int i = 0, j = 1; i < arr.length - 1; i++, j++) {
             if (arr[i].compareTo(arr[j]) > 0) {
@@ -26,6 +43,7 @@ public class Util {
         }
         return true;
     }
+
 
     public static void exchange(int[] arr, int i, int j) {
         int t = arr[i];
@@ -40,7 +58,22 @@ public class Util {
     }
 
 
-    private static Random generator = new java.util.Random();
+    public static double median(int[] arr, int begin, int size) {
+        int m = begin + (size - 1) / 2;
+        // odd
+        if (size % 2 == 1) {
+            return arr[m];
+        } else {
+            // even then (arr[n/2 - 1] + arr[n/2])/2.0
+            return (arr[m] + arr[m + 1]) / 2.0;
+        }
+    }
+
+
+    public static double median(int[] arr) {
+        return median(arr, 0, arr.length);
+    }
+
 
     public static int[] getRandomArray(int length) {
         int[] arr = new int[length];
@@ -62,17 +95,22 @@ public class Util {
         return (sum(nums) * 1.0) / nums.length;
     }
 
+    public static int max(int[] nums, int begin, int size) {
+        int end = begin + size;
+        int m = Integer.MIN_VALUE;
+        for (int i = begin; i < end; i++) {
+            if (nums[i] > m) m = nums[i];
+        }
+        return m;
+    }
+
     /**
-     * takes an array of integers
+     * Finds the largest element in an array of integers.
      *
      * @return index of the largest element in the array
      */
     public static int max(int[] nums) {
-        int m = Integer.MIN_VALUE;
-        for (int num : nums) {
-            if (num > m) m = num;
-        }
-        return m;
+        return max(nums, 0, nums.length);
     }
 
     public static class Stopwatch {
--- src/test/java/io/codelavida/UtilTest.java
+++ src/test/java/io/codelavida/UtilTest.java
@@ -2,15 +2,15 @@ package io.codelavida;
 
 import org.junit.Test;
 
-import static io.codelavida.Util.isSorted;
-import static io.codelavida.Util.max;
+import static io.codelavida.Util.*;
 import static org.junit.Assert.*;
 
 public class UtilTest {
 
     @Test
-    public void testMax() throws IllegalAccessException {
+    public void testMax() {
         assertEquals(91, max(new int[]{12, 10, 4, 7, 91}));
+        assertEquals(10, max(new int[]{12, 10, 4, 7, 91}, 1, 2));
     }
 
     @Test
@@ -18,6 +18,19 @@ public class UtilTest {
         assertTrue(isSorted(new int[]{2, 4, 10, 45, 91}));
         assertFalse(isSorted(new int[]{12, 10, 4, 7, 91}));
     }
+
+    @Test
+    public void testMedianEntire() {
+        assertEquals(17.0, median(new int[]{2, 5, 17, 21, 32}), 0.001);
+        assertEquals(19.5, median(new int[]{2, 5, 17, 22, 32, 43}), 0.001);
+    }
+
+    @Test
+    public void testMedianPart() {
+        assertEquals(32.0, median(new int[]{2, 5, 17, 22, 32, 43, 78, 97}, 2, 5), 0.001);
+        assertEquals(27.0, median(new int[]{2, 5, 17, 22, 32, 43, 78, 97}, 2, 4), 0.001);
+    }
+
 
 
 }
