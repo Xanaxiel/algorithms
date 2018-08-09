eeec2264e94ba23ad0b2a62e4a690dfe2a6a50a4
Rabi Panda <rabipanda@icloud.com>
2018-08-09 16:11:02 +1000

    Use Google truth assertions for testing

--- src/test/java/io/codelavida/ds/ArrayListTest.java
+++ src/test/java/io/codelavida/ds/ArrayListTest.java
@@ -2,27 +2,27 @@ package io.codelavida.ds;
 
 import org.junit.Test;
 
-import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static com.google.common.truth.Truth.assertThat;
+
 
 public class ArrayListTest {
 
     @Test
     public void sizeOfEmptyListMustBeZero() {
         List<Integer> list = new ArrayList<>();
-        assertEquals(0, list.size());
+        assertThat(list).hasSize(0);
     }
 
     @Test
     public void elementShouldBeAddedAtEnd() {
         List<Integer> list = new ArrayList<>();
+
         list.add(10);
-        assertEquals(1, list.size());
-        assertEquals(Integer.valueOf(10), list.get(0));
+
+        assertThat(list.size()).isEqualTo(1);
+        assertThat(list.get(0)).isEqualTo(10);
     }
 
     @Test
@@ -36,44 +36,51 @@ public class ArrayListTest {
 
         list2.addAll(list1);
 
-        assertEquals(3, list2.size());
+        assertThat(list2).hasSize(3);
     }
 
     @Test
     public void elementShouldBeAddedByPosition() {
         List<Integer> list = new ArrayList<>();
+
         list.add(10);
-        assertEquals(1, list.size());
-        assertEquals(Integer.valueOf(10), list.get(0));
+
+        assertThat(list.size()).isEqualTo(1);
+        assertThat(list.get(0)).isEqualTo(10);
     }
 
     @Test
     public void elementShouldReplace() {
         List<Integer> list = new ArrayList<>();
+
         list.add(10);
-        assertEquals(Integer.valueOf(10), list.set(0, 20));
-        assertEquals(Integer.valueOf(20), list.get(0));
+
+        assertThat(list.set(0, 20)).isEqualTo(10);
+        assertThat(list.get(0)).isEqualTo(20);
     }
 
     @Test
     public void elementShouldBeRemovedByPosition() {
         List<Integer> list = new ArrayList<>();
+
         list.add(10);
         list.add(20);
         list.add(30);
-        assertEquals(Integer.valueOf(20), list.remove(1));
-        assertEquals(2, list.size());
+
+        assertThat(list.remove(1)).isEqualTo(20);
+        assertThat(list.size()).isEqualTo(2);
     }
 
     @Test
     public void elementShouldBeRemovedByValue() {
         List<Integer> list = new ArrayList<>();
+
         list.add(10);
         list.add(30);
-        assertTrue(list.remove(Integer.valueOf(30)));
-        assertEquals(1, list.size());
-        assertFalse(list.remove(Integer.valueOf(20)));
-        assertEquals(1, list.size());
+
+        assertThat(list.remove(Integer.valueOf(30))).isTrue();
+        assertThat(list.size()).isEqualTo(1);
+        assertThat(list.remove(Integer.valueOf(20))).isFalse();
     }
 
     @Test
@@ -81,20 +88,25 @@ public class ArrayListTest {
         List<Integer> list = new ArrayList<>();
         list.add(10);
         list.add(20);
-        assertTrue(list.contains(20));
-        assertFalse(list.contains(30));
+
+        assertThat(list).contains(20);
+        assertThat(list).doesNotContain(30);
     }
 
     @Test
     public void containmentOfElementsInList() {
         List<Integer> list = new ArrayList<>();
+
         list.add(10);
         list.add(20);
         list.add(30);
+
         List<Integer> subList = new ArrayList<>();
+
         subList.add(20);
         subList.add(30);
-        assertTrue(list.containsAll(subList));
+
+        assertThat(list.containsAll(subList)).isTrue();
     }
 
 }
