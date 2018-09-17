42e721e5193b0f7054c21bc634374a4492d000f5
Rabi Panda <rabipanda@icloud.com>
2018-09-17 20:19:14 +1000

    Add Clojure

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject algorithms "0.1.0-SNAPSHOT"
+  :description "Algorithms in Clojure"
+  :url "https://github.com/codelavida/algorithms"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
---
+++ src/algo_clj/core.clj
@@ -0,0 +1,2 @@
+(ns algo-clj.core)
+
